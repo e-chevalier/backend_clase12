acce2e192a5a5f687addcf03acca5a6580025944
e-chevalier <echevalier.dev@gmail.com>
2022-03-22 10:06:17 -0300

    Add Desafio class 11

--- practicas/server.js
+++ practicas/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import { serverRoutes } from './routes/index.js'
 import sessionFileStore from 'session-file-store'
+import MongoStore from 'connect-mongo'
 
 
 
@@ -19,8 +20,26 @@ app.use(cors("*"))
 
 let FileStore = sessionFileStore(session)
 
+// app.use(session({
+//     store: new FileStore({path: '../sesiones', ttl:300, retries: 0}),
+//     secret: 'secreto',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {maxAge: 10000}
+// }))
+
+const advanceOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+
+const DB_PASS = config.db_pass
+const DB_DOMAIN = config.db_domain
+const DB_NAME = config.db_name
+const DB_USER = config.db_user
+
 app.use(session({
-    store: new FileStore({path: '../sesiones', ttl:300, retries: 0}),
+    store: MongoStore.create({
+        mongoUrl: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_DOMAIN}/${DB_NAME}?retryWrites=true&w=majority`,
+        mongoOptions: advanceOptions
+    }),
     secret: 'secreto',
     resave: false,
     saveUninitialized: false,
